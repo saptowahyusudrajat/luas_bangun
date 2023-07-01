cd89437775314a040dee270fbbd760d457282e97
saptosudrajat <saptowahyusudrajat@gmail.com>
2023-07-01 11:50:56 +0700

    first project alat hitung luas bangun 2 dimensi

---
+++ tesaplikasi.py
@@ -0,0 +1,7 @@
+def luas_segitiga():
+    a = int(input("Masukan alas segitiga: "))
+    t = int(input("masukkan tinggi segitiga: "))
+    luas = a*t/2
+    print("Luas segitiga adalah: ", luas)
+
+luas_segitiga()
