87ccadf9adc55b70aa57d91e562d4cfa73df5ae7
saptowahyusudrajat <mtvgsapto@gmail.com>
2023-07-01 12:22:08 +0700

    Ditambahkan persamaan unutk menghitung luas persegi panjang

--- tesaplikasi.py
+++ tesaplikasi.py
@@ -5,3 +5,11 @@ def luas_segitiga():
     print("Luas segitiga adalah: ", luas)
 
 luas_segitiga()
+
+def luas_persegi_panjang():
+    p = int(input("masukan panjang: "))
+    l = int(input("masukkan lebar: "))
+    luas = p*l
+    print ("luas persegi panjang adalah: ", luas)
+
+luas_persegi_panjang()
